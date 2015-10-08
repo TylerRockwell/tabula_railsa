e40c762fa403f0a375138bc49d7e3c5bd9b25958
Tyler Rockwell <tyler.rockwell87@gmail.com>
2015-10-08 10:59:42 -0400

    Challenge 10-08-15

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,30 @@
+class AnswersController < ApplicationController
+  before_action :set_answer, only: [:show]
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      redirect_to @answer, notice: "Answers saved"
+    else
+      redirect_to home_new_path
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+    def answer_params
+      params.require(:answer).permit(:name, :chocolate, :puppies, :rainbows, :cold_hard_cash)
+    end
+
+    def set_answer
+      @answer = Answer.find(params[:id])
+    end
+
+end
